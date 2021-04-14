72a7d92a954791f4bd72ad817ce7be35407a03f2
butsuli-shine <cheichey.1223@gmail.com>
2021-04-15 00:14:20 +0900

    add: ラジオボタンのイベントリスナー, APIをたたく処理を追加した

--- src/components/radioButtons.jsx
+++ src/components/radioButtons.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { getPopulationComposition } from '../api'
 
 const RadioButtons = (props) => {
-    const {prefectureData} = props;
-    return (
-    prefectureData?.map(
-        (d, index) => {
-            return(
-                <li key={`data_${index}`}>{d?.prefName} <input type="radio" /></li>
-            )
+    const {prefectureData, setPopulationData} = props;
+
+    const handleRadioButtons = (e) => {
+        const targetKey = e.currentTarget.dataset.key;
+        const get = async () => {
+            const result = await getPopulationComposition(targetKey);
+            setPopulationData(result.data.result.data[0]);
         }
-    )
+        get();
+
+    }
+    return (
+        prefectureData?.map(
+            (d, index) => {
+                return(
+                    <li >{d?.prefName} <input type="radio" data-key={index + 1} onChange={(e) => handleRadioButtons(e)} /></li>
+                )
+            }
+        )
+
     )
 
 }
