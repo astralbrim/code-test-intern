944cc99304fed4ce14f8a0813bc1836be1fcbf99
butsuli-shine <cheichey.1223@gmail.com>
2021-04-16 01:34:20 +0900

    add: 図を表示するための一連の処理を追加した

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -1,9 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
-const Chart = () => {
+const Chart = (props) => {
+    const {populationData} = props;
+    const creatOptions = (series) => {
+        return (
+            {
+                title: {
+                    text: "人口構成"
+                },
+                xAxis: {
+                    title: {
+                        text: "年"
+                    }
+                },
+                yAxis: {
+                    title: {
+                        text: "人口"
+                    }
+                },
+                plotOptions: {
+                    series: {
+                        label: {
+                            connectorAllowed: false
+                        },
+                        pointInterval: 5,
+                        pointStart: 1960,
+                    },
+                },
+                series: series
+            }
+        )
+    }
+    const [series, setSeries] = useState([]);
+    const [options, setOptions] = useState({});
+
+    // populationData に変化があったとき series を更新する
+    useEffect(
+        () => {
+            const tmp = populationData.map(
+                (value1) => {
+                    console.log(value1);
+                    return (
+                        value1.data.map(
+                            (value2) => {
+                                return value2.value;
+                            }
+                        )
+                    );
+                }
+            )
+            setSeries([...series, tmp]);
+            console.log(series);
+        },[populationData]
+    )
+
+    // series に変化があったとき optionsを更新する
+    useEffect(
+        () => {
+            setOptions(creatOptions(series));
+        },[series]
+    )
     return (
         <div>
-            図
+            <HighchartsReact highcharts={Highcharts} options={options}/>
         </div>
     )
 }
