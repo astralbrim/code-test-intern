f32c4ab25b813ab7ce0e6af14497f1e61f586e84
butsuli-shine <cheichey.1223@gmail.com>
2021-04-15 20:28:27 +0900

    fix: typoを修正

--- src/components/App.js
+++ src/components/App.js
@@ -25,7 +25,7 @@ function App() {
         <div className="App">
             {console.log(populationData)}
             <Header/>
-            <RadioButtons prefectureData={prefectureData} populationData={{populationData}} setPupulationData={setPopulationData}/>
+            <RadioButtons prefectureData={prefectureData} populationData={populationData} setPopulationData={setPopulationData}/>
             <Chart populationData={populationData}/>
         </div>
     )
