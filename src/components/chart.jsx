c1ca2ec73b2ea0e4d73a81bf0142c507c613f25b
butsuli-shine <cheichey.1223@gmail.com>
2021-04-16 10:44:06 +0900

    add: lastPopulationData を追加し, populationData が追加されたのか削除されたのか判定する処理を追加した

--- src/components/chart.jsx
+++ src/components/chart.jsx
@@ -33,25 +33,32 @@ const Chart = (props) => {
             }
         )
     }
+    const [lastPopulationData, setLastPopulationData] = useState([])
     const [series, setSeries] = useState([]);
     const [options, setOptions] = useState({});
 
     // populationData に変化があったとき series を更新する
     useEffect(
         () => {
-            console.log(populationData.slice(-1)[0])
-            if (populationData.slice(-1)[0] !== undefined){
-                const tmp = populationData.slice(-1)[0].data.map(
-                    (value) => {
-                        return value.value;
-                    }
-                )
-                setSeries([...series, tmp]);
-                console.log(series);
+            if (populationData.slice(-1)[0] !== undefined) { // 最初は配列の要素が undefinedなら無視
+                console.log(lastPopulationData, populationData);
+                const lastPopulationDataLength = lastPopulationData.length;
+                console.log(lastPopulationData.length, populationData.length);
+                setLastPopulationData(populationData); // 一個前のpopulationData を更新
+                if (lastPopulationDataLength < populationData.length) { // populationData に要素が追加された時の処理
+                    const tmp = populationData.slice(-1)[0].data.map( // 配列の末尾要素の参照
+                        (value) => {
+                            return value.value;
+                        }
+                    )
+                    setSeries([...series, tmp]);
+                    console.log(series);
+                } else { // populationData の要素が削除されたとき
+                        setSeries(populationData.pop());
+                }
             }
-
-        },[populationData]
-    )
+            }, [populationData]
+    );
 
     // series に変化があったとき optionsを更新する
     useEffect(
